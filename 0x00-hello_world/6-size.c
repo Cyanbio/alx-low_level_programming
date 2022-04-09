b520a2f06a23c723a60253131685f837e2744bae
Cyanbio <siyanbola_tayo@yahoo.com>
2022-04-09 13:09:33 -0700

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-printf("size of a char: %c byte(s)", sizeof(char));
-printf("size of an int: %i byte(s)", sizeof(int));
-printf("size of a long int: %ld byte(s)", sizeof(long int));
+printf("size of a char: %c byte(s)\n", sizeof(char));
+printf("size of an int: %i byte(s)\n", sizeof(int));
+printf("size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("size of a long long int: %lld byte(s)\n", sizeof(long long int));
 printf("size of a float: %f byte(s)\n", sizeof(float));
 return (0)
