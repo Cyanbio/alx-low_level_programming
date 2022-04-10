b6ad1afe14f1ec588e94105edff8276c9675e0b7
Cyanbio <siyanbola_tayo@yahoo.com>
2022-04-10 09:53:55 -0700

    five

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,9 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int num;
