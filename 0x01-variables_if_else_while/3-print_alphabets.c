04024ede316293b4a18f4d95ce24f390ab2c6551
Cyanbio <siyanbola_tayo@yahoo.com>
2022-04-10 09:58:53 -0700

    me

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,43 +1,23 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+char alpha;
+char bet;
 
-	char alpha;
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+{
+putchar(alpha);
+}
+for (bet = 'A' ; bet <= 'Z' ; bet++)
+{
+putchar(bet);
+}
+putchar('\n');
 
-	char bet;
-
-
-
-	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
-
-	{
-
-		putchar(alpha);
-
-	}
-
-	for (bet = 'A' ; bet <= 'Z' ; bet++)
-
-	{
-
-		putchar(bet);
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+return (0);
 }
